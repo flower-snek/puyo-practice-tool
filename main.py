8b73d64ea914fb2418e5439f466cc9426f0a9dc0
ahempe02 <boomerangbro12@gmail.com>
2024-02-07 19:32:51 -0500

    popping puyos and chaining combos

--- main.py
+++ main.py
@@ -12,6 +12,11 @@ PUYO_COLORS = [pygame.Color(30, 30, 30), pygame.Color(80, 40, 40), pygame.Color(
 PUYO_FALL_TIME = 32
 DAS = 8  # Delayed Auto Shift
 ARR = 2  # Auto Repeat Rate
+DROP_TIMES = [10, 5, 4, 3, 3, 3, 3, 2, 2, 2, 2, 2, 2, 2]
+TIME_BETWEEN_PUYOS = 16
+
+TIME_TO_POP = 48 # long animation...
+GROUP_TO_POP = 4  # 4 is default ofc, but other numbers can be ""fun""
 
 
 def collision_logic():
@@ -43,7 +48,7 @@ def rotate_logic(dir):
     global rotation
     # handles logic for rotations (mainly wall kicks, but also can't rotate once in a one wide well)
     # first do the rotation and pretend it's fine
-    rotation += dir
+    rotation = (rotation + dir) % 4
     # then check if it's fine
     # if the other puyo isn't in a wall right now, it's fine
     # print(other_puyo_pos()[1])
@@ -59,19 +64,75 @@ def rotate_logic(dir):
         if canMove(-1):
             puyo_pos[0] -= 1
         else:
-            rotation -= dir
+            rotation = (rotation - dir) % 4
         return
 
     if rotation == 3:
         if canMove(1):
             puyo_pos[0] += 1
         else:
-            rotation -= dir
+            rotation = (rotation - dir) % 4
         return
 
     # if somehow something else happens..... let's just cancel the rotation. be safe.
-    rotation -= dir
+    rotation = (rotation - dir) % 4
     return
+
+
+def puyos_need_drop():
+    dropped = False
+    for y in range(GRID_SIZE[1]+HIDDEN_ROWS - 2, -1, -1):
+        for x in range(GRID_SIZE[0]):
+            if board_at([x, y]) != 0 and board_at([x, y+1]) == 0:
+                # get that puyo down from there!!
+                board_state[x][y+1] = board_state[x][y]
+                board_state[x][y] = 0
+                dropped = True
+    return dropped
+
+
+def flag_pop_puyos():
+    pop_count = 0
+    for y in range(GRID_SIZE[1]+HIDDEN_ROWS):
+        for x in range(GRID_SIZE[0]):
+            if board_at([x, y]) != 0:
+                # this is probably really bad but i dont know a better way to do it and i dont exactly wanna copy code off the internet and its a small enough field that it works fine!!
+                current_group = board_at([x, y])
+                group_size = 1
+                checked_stack = [[x, y]]
+                check_stack = [[x + 1, y], [x - 1, y], [x, y + 1], [x, y - 1]]
+                while len(check_stack) > 0:
+                    cur_check = check_stack.pop()
+                    if 0 <= cur_check[0] < GRID_SIZE[0] and 0 <= cur_check[1] < GRID_SIZE[1] + HIDDEN_ROWS:
+                        if board_at(cur_check) == current_group:
+                            group_size += 1
+                            checked_stack.append(cur_check)
+                            new_additions = [[cur_check[0] + 1, cur_check[1]], [cur_check[0] - 1, cur_check[1]],
+                                             [cur_check[0], cur_check[1] + 1], [cur_check[0], cur_check[1] - 1]]
+                            for i in new_additions:
+                                unique = True
+                                for j in check_stack:
+                                    if j[0] == i[0] and j[1] == i[1]:
+                                        unique = False
+                                for j in checked_stack:
+                                    if j[0] == i[0] and j[1] == i[1]:
+                                        unique = False
+                                if unique:
+                                    check_stack.append(i)
+                # print(x, y, group_size)
+
+                if group_size >= 4:
+                    popping_board[x][y] = True
+                    pop_count += 1
+    return pop_count
+
+
+def perform_pop_puyos():
+    for y in range(GRID_SIZE[1] + HIDDEN_ROWS):
+        for x in range(GRID_SIZE[0]):
+            if popping_board[x][y]:
+                board_state[x][y] = 0
+                popping_board[x][y] = False
 
 
 if __name__ == "__main__":
@@ -82,6 +143,9 @@ if __name__ == "__main__":
 
     dt = 0  # everything will probably be frame-based but just in case
     puyo_pos = [2, 1]
+    pop_delay = 0
+    drop_delay = 0
+    drop_step = 0
     fall_timer = PUYO_FALL_TIME
     das_timers = [0, 0]
     das_active = [False, False]
@@ -89,6 +153,7 @@ if __name__ == "__main__":
     cur_puyos = (random.randint(1, 4), random.randint(1, 4))
     rotation = 0  # 0-3, up-right-down-left probably
     board_state = [[0 for y in range(GRID_SIZE[1]+HIDDEN_ROWS)] for x in range(GRID_SIZE[0])]
+    popping_board = [[False for y in range(GRID_SIZE[1] + HIDDEN_ROWS)] for x in range(GRID_SIZE[0])]
     while running:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
@@ -101,7 +166,7 @@ if __name__ == "__main__":
             if das_timers[i] > 0:
                 das_timers[i] -= 1
         if keys[pygame.K_LEFT]:
-            if das_timers[0] == 0:
+            if das_timers[0] == 0 and drop_delay == 0:
                 if das_active[0]:
                     das_timers[0] = ARR
                 else:
@@ -112,7 +177,7 @@ if __name__ == "__main__":
         elif das_active[0]:
             das_active[0] = False
         if keys[pygame.K_RIGHT]:
-            if das_timers[1] == 0:
+            if das_timers[1] == 0 and drop_delay == 0:
                 if das_active[1]:
                     das_timers[1] = ARR
                 else:
@@ -123,7 +188,7 @@ if __name__ == "__main__":
         elif das_active[1]:
             das_active[1] = False
         if keys[pygame.K_DOWN]:
-            fall_timer -= 15
+            fall_timer -= (PUYO_FALL_TIME/2) - 1  # 2f fastfall
 
         # temporary reset board button
         if keys[pygame.K_r]:
@@ -141,21 +206,49 @@ if __name__ == "__main__":
             rotation_active[1] = True
 
         # ## LOGIC STUFF GOES HERE ## #
+        if pop_delay > 1:
+            pop_delay -= 1
+        elif pop_delay == 1:
+            pop_delay = 0
+            perform_pop_puyos()
+            if puyos_need_drop():
+                drop_delay = DROP_TIMES[drop_step]
+                drop_step += 1
+        elif drop_delay > 0:
+            drop_delay -= 1
+        elif drop_step > 0:
+            if puyos_need_drop():
+                drop_delay = DROP_TIMES[drop_step]
+                drop_step += 1
+            else:
+                if flag_pop_puyos() > 0:
+                    pop_delay = TIME_TO_POP
+                drop_delay = TIME_BETWEEN_PUYOS
+                drop_step = 0
+        else:
+            fall_timer -= 1
+            if fall_timer <= 0:
+                fall_timer = PUYO_FALL_TIME
+                puyo_pos[1] += 1
 
-        fall_timer -= 1
-        if fall_timer <= 0:
-            fall_timer = PUYO_FALL_TIME
-            puyo_pos[1] += 1
-
-        if collision_logic():
-            # print(puyo_pos)
-            # print(other_puyo_pos)
-            puyo_pos[1] -= 1
-            board_state[puyo_pos[0]][puyo_pos[1]] = cur_puyos[0]
-            board_state[other_puyo_pos()[0]][other_puyo_pos()[1]] = cur_puyos[1]
-            # generate new puyo
-            puyo_pos = [2, 1]
-            cur_puyos = (random.randint(1, 4), random.randint(1, 4))
+            if collision_logic():
+                # print(puyo_pos)
+                # print(other_puyo_pos)
+                puyo_pos[1] -= 1
+                board_state[puyo_pos[0]][puyo_pos[1]] = cur_puyos[0]
+                board_state[other_puyo_pos()[0]][other_puyo_pos()[1]] = cur_puyos[1]
+                # generate next puyo
+                puyo_pos = [2, 1]
+                cur_puyos = (random.randint(1, 4), random.randint(1, 4))
+                rotation = 0
+                if puyos_need_drop():
+                    drop_delay = DROP_TIMES[drop_step]
+                    drop_step += 1
+                else:
+                    if flag_pop_puyos() > 0:
+                        pop_delay = TIME_TO_POP
+                    drop_delay = TIME_BETWEEN_PUYOS
+                    drop_step = 0
 
         # ## DRAW STUFF GOES HERE ## #
 
@@ -166,15 +259,20 @@ if __name__ == "__main__":
         grid_pixelsize = (GRID_SIZE[0] * square_size, GRID_SIZE[1] * square_size)
         base_pos = ((screen.get_width() - grid_pixelsize[0])/2, (screen.get_height() - grid_pixelsize[1])/2)
 
+        pop_highlight = int(((TIME_TO_POP - pop_delay) * 50) / TIME_TO_POP)
+        # print(pop_highlight)
         for y in range(HIDDEN_ROWS, GRID_SIZE[1] + HIDDEN_ROWS):
             ypos = base_pos[1] + square_size * (y - HIDDEN_ROWS)
             for x in range(GRID_SIZE[0]):
                 xpos = base_pos[0] + square_size * x
                 fill = PUYO_COLORS[board_state[x][y]]
-                if x == puyo_pos[0] and y == puyo_pos[1]:
-                    fill = PUYO_COLORS[cur_puyos[0]]
-                elif x == other_puyo_pos()[0] and y == other_puyo_pos()[1]:
-                    fill = PUYO_COLORS[cur_puyos[1]]
+                if popping_board[x][y]:
+                    fill = fill + pygame.Color(pop_highlight, pop_highlight, pop_highlight)
+                if pop_delay == 0 and drop_delay == 0 and drop_step == 0:
+                    if x == puyo_pos[0] and y == puyo_pos[1]:
+                        fill = PUYO_COLORS[cur_puyos[0]]
+                    elif x == other_puyo_pos()[0] and y == other_puyo_pos()[1]:
+                        fill = PUYO_COLORS[cur_puyos[1]]
                 pygame.draw.rect(screen, fill, pygame.Rect(xpos, ypos, square_size*0.9, square_size*0.9))
 
         pygame.display.flip()
